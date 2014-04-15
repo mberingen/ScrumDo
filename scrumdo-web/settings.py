2a2f804dd6dfeb4dce2e01da7976c3f550009823
Marc Beringen <mberingen@gmail.com>
2014-04-15 11:05:23 +0200

    use 127.0.0.1:8000 instead of localhost

--- scrumdo-web/settings.py
+++ scrumdo-web/settings.py
@@ -275,8 +275,8 @@ CACHE_BACKEND = 'locmem://'
 
 INTERNAL_IPS = ('127.0.0.1',)
 
-BASE_URL = "http://localhost:8000"
-SSL_BASE_URL = "http://localhost:8000"
+BASE_URL = "http://127.0.0.1:8000"
+SSL_BASE_URL = "http://127.0.0.1:8000"
 
 SCRUMDO_EXTRAS = ()
  #"extras.plugins.github_issues.GitHubIssuesExtra",
