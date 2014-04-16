0073d9446b9fe4ed3a4a7b7939abb1654337e13c
Marc Beringen <mberingen@gmail.com>
2014-04-16 15:30:26 +0200

    moved to local settings

--- scrumdo-web/settings.py
+++ scrumdo-web/settings.py
@@ -274,9 +274,6 @@ CACHE_BACKEND = 'locmem://'
 
 INTERNAL_IPS = ('127.0.0.1',)
 
-BASE_URL = "http://127.0.0.1:8000"
-SSL_BASE_URL = "http://127.0.0.1:8000"
-
 SCRUMDO_EXTRAS = ()
  #"extras.plugins.github_issues.GitHubIssuesExtra",
  #                 "extras.plugins.example.ExampleExtra",)
